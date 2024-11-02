f6b4b152830107e86f2fae18941e40e2669ed515
Rebecca Mewse <wkcg77@durham.ac.uk>
2024-11-02 22:39:20 +0000

    Hand recog

---
+++ Src/hand_recog.py
@@ -0,0 +1,30 @@
+import cv2
+import numpy as np
+from recognition.py import cam
+
+# Function to recognise movement of boxes
+def recog_gesture(prev_center, cur_center):
+    
+    # Check if there is a previous center
+    if prev_center is None:
+        return None
+    
+    # Get movement of hand
+    dx = cur_center[0] - prev_center[0]
+    dy = cur_center[1] - prev_center[1]
+    
+    # Threshold for the amount of pixels the hand has to move to be recognised
+    threshold = 100
+    if abs(dy) < abs(dx): # This is a horizontal movement, we need to distinguish between left/right
+        if dx > threshold:
+            return "Right"
+        elif dx < -threshold:
+            return "Left"
+    else: # This is a vertical movement
+        if dy > threshold:
+            return "Down"
+        elif dy < -threshold:
+            return "Up"
+        
+    # If none of the conditions for direction recognition
+    return none
